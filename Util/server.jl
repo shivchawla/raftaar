d873a4b73963adf7aa5eff8b0e7d2f16b67fdd3f
Shiv Chawla <shivchawla2001@gmail.com>
2017-10-02 10:16:00 +0530

    Added error handling and proper closing of connections

--- Util/server.jl
+++ Util/server.jl
@@ -26,26 +26,60 @@ function decodeMessage(msg)
 end
 
 global fname = ""
+global tf = ""
 global parsed_args = Dict{String, Any}()
+
+function remove_files()
+    try
+        #remove the tempfile after completion
+        rm(tf)
+        rm(fname)
+        rm("$tmpdir/handleErrors.jl")
+        rm("$tmpdir/Run_Algo.jl")
+        rm("$tmpdir/*")
+    end
+end
+
+function close_connection(client)
+    remove_files()
+    delete!(connections, client.id)
+    close(client)
+end
 
 wsh = WebSocketHandler() do req, client
     
+    #check if there are no connections
+    if(length(collect(keys(connections)))) > 0)
+       msg = Dict{String, Any}("msg" => "Server Unavailable", "code" => 503, "outputtype" => "internal");
+       write(client, JSON.json(msg))
+       close_connection(client) 
+    end
+    
+    remove_files()
+
+    #continue otherewise
     connections[client.id] = client
 
-    setlogmode(:json, :socket, true, client)
+    try 
+        setlogmode(:json, :socket, true, client)
 
-    msg = read(client)
-    argsString = decodeMessage(msg)
-    args = [String(ss) for ss in split(argsString,"??##")]
+        msg = read(client)
+        argsString = decodeMessage(msg)
+        args = [String(ss) for ss in split(argsString,"??##")]
 
-    info_static("Starting Backtest")
+        info_static("Starting Backtest")
 
-    # Parse arguments from the connection message.
-    info_static("Parsing arguments from settings panel")
-    global parsed_args = parse_arguments(args)
+        # Parse arguments from the connection message.
+        info_static("Parsing arguments from settings panel")
+        global parsed_args = parse_arguments(args)
 
-    parseError = false
-    info_static("Processing parsed arguments from settings panel")
+        parseError = false
+        info_static("Processing parsed arguments from settings panel")
+    catch err
+        info_static("Internal Error while processing settings")
+        close_connection(client)
+        return
+    end
     
     try
         global fname = processargs(parsed_args)
@@ -58,7 +92,7 @@ wsh = WebSocketHandler() do req, client
             _outputbacktestlogs()
         end
 
-        close(client)
+        close_connection(client)
         return
     end
 
@@ -71,38 +105,45 @@ wsh = WebSocketHandler() do req, client
             #_outputbackteststatistics()
             _outputbacktestlogs()
         end
-        close(client)
+        close_connection(client)
         return
     end
-
-    #create a temporary file to copy all the relevant code required
-    #to run the backtest
-    (tf, io) = mktemp(tmpdir)
-    close(io)
-    
-    cp(Base.source_dir()*"/handleErrors.jl", "$tmpdir/handleErrors.jl", remove_destination=true)
-    cp(Base.source_dir()*"/Run_Algo.jl", "$tmpdir/Run_Algo.jl", remove_destination=true)
-
-    #copy the boilerplate code
-    #includes relevant modules and create db connections
-    cp(Base.source_dir()*"/boilerPlate.jl", tf, remove_destination=true)
-
-    #Append user source code to the fle
-    open(tf, "a") do f
-        open(fname, "r") do ff
-            source_code = readlines(ff)
-            for l in source_code
-                write(f, "\n$l")
-            end                
-        end
+   
+    try
+        #create a temporary file to copy all the relevant code required
+        #to run the backtest
+        (tf, io) = mktemp(tmpdir)
+        global tf = tf
+        close(io)
         
-        #Append the actual backtesting function
-        if parsed_args["forward"] 
-            write(f, "\nrun_algo(true)")
-        else
-            write(f, "\nrun_algo(false)")
+        cp(Base.source_dir()*"/handleErrors.jl", "$tmpdir/handleErrors.jl", remove_destination=true)
+        cp(Base.source_dir()*"/Run_Algo.jl", "$tmpdir/Run_Algo.jl", remove_destination=true)
+
+        #copy the boilerplate code
+        #includes relevant modules and create db connections
+        cp(Base.source_dir()*"/boilerPlate.jl", tf, remove_destination=true)
+
+        #Append user source code to the fle
+        open(tf, "a") do f
+            open(fname, "r") do ff
+                source_code = readlines(ff)
+                for l in source_code
+                    write(f, "\n$l")
+                end                
+            end
+            
+            #Append the actual backtesting function
+            if parsed_args["forward"] 
+                write(f, "\nrun_algo(true)")
+            else
+                write(f, "\nrun_algo(false)")
+            end
+            write(f, "\nAPI.reset()")
         end
-        write(f, "\nAPI.reset()")
+    catch err
+        info_static("Internal Error")
+        close_connection(client)
+        return
     end
 
     #Run the complete file
@@ -117,14 +158,8 @@ wsh = WebSocketHandler() do req, client
         println(err)
     end
 
-    #remove the tempfile after completion
-    rm(tf)
-    rm(fname)
-    rm("$tmpdir/handleErrors.jl")
-    rm("$tmpdir/Run_Algo.jl")
-
     #close the ws client on successful completion
-    close(client)
+    close_connection(client)
 
 end
 
