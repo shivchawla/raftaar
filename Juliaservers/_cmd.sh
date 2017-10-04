f1a08c9c5cf914820a1d026f70e9aa5a2aff8aed
Shiv Chawla <shivchawla2001@gmail.com>
2017-10-04 17:45:16 +0530

    Another missed check-in cmd.sh (change directory)

--- Juliaservers/_cmd.sh
+++ Juliaservers/_cmd.sh
@@ -1,9 +1,9 @@
 # @Author: Shiv Chawla
 # @Date:   2017-10-04 12:19:42
 # @Last Modified by:   Shiv Chawla
-# @Last Modified time: 2017-10-04 12:27:26
+# @Last Modified time: 2017-10-04 17:44:49
 
 #!/bin/bash
 julia="/root/julia/bin/julia"
 user="jp_$1"
-su - $user -c "$julia /root/aimsquant/raftaar/Util/server.jl $1 0.0.0.0"
+su - $user -c "$julia /root/aimsquant/raftaar/Util/Run/server.jl $1 0.0.0.0"
