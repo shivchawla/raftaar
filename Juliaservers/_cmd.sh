c029b02ab423ed9dbdd7f72cfd5735dbb445bbe9
Shiv Chawla <shivchawla2001@gmail.com>
2017-10-08 15:39:42 +0530

    Directory structure and link change :(

--- Juliaservers/_cmd.sh
+++ Juliaservers/_cmd.sh
@@ -1,9 +1,10 @@
 # @Author: Shiv Chawla
 # @Date:   2017-10-04 12:19:42
 # @Last Modified by:   Shiv Chawla
-# @Last Modified time: 2017-10-08 15:03:08
+# @Last Modified time: 2017-10-08 15:38:59
 
 #!/bin/bash
-julia="/admin/home/julia/bin/julia"
+base_dir="/home/admin"
+julia="$base_dir/julia/bin/julia"
 user="jp_$1"
-su - $user -c "$julia /admin/home/raftaar/Util/Run/server.jl $1 $2"
+su - $user -c "$julia $base_dir/raftaar/Util/Run/server.jl $1 $2"
