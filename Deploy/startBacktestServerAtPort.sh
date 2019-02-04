047c58fee5b27cebc44fb77add56386fb86018dc
Shiv Chawla <shivchawla2001@gmail.com>
2019-02-04 16:48:20 +0530

    Add write permissions to julia user to .julia folder

--- Deploy/startBacktestServerAtPort.sh
+++ Deploy/startBacktestServerAtPort.sh
@@ -23,7 +23,7 @@ chmod -R u=rx /home/$user/raftaar
 
 chown -R $user /home/$user/.julia
 chgrp -R $user /home/$user/.julia
-chmod -R u=rx /home/$user/.julia
+chmod -R u=rwx /home/$user/.julia
 
 mkdir -p /home/$user/.julia/config && cp /home/admin/$env/raftaar/Deploy/startup.jl /home/$user/.julia/config/startup.jl --force
 mkdir -p /home/$user/.julia/environments/v1.1/ && cp /home/admin/$env/raftaar/Manifest.toml /home/$user/.julia/environments/v1.1/Manifest.toml --force
